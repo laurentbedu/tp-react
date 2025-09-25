e8e85834baf140a3b2af4614da3d593bdd29cc14
laurentbedu <bedulaurent@gmail.com>
2025-09-25 10:31:23 +0200

    WiP

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import "./App.css";
-import AppForExercices from "./AppForExercices";
-import AppForExerciceRecap from "./AppForExercieRecap";
-
-//FIXME: placeholder.com n'existe plus, utilisez placehold.co pour les images placeholder 
 
 function App() {
   return (
-    //TODO: commenter ou décommenter les composants pour voir la corrections des exercices 1 à 6 ou de l'exercice récapitulatif
     <div className="App">
-      {/* <AppForExercices /> */}
-      <AppForExerciceRecap />
+
     </div>
   );
 }
