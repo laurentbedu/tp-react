e8152a083b4e4421dbbea0c54c957a83e3f46c6c
laurentbedu <bedulaurent@gmail.com>
2025-09-25 11:12:21 +0200

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import "./App.css";
-import AppForExercices from "./AppForExercices";
-import AppForExerciceRecap from "./AppForExercieRecap";
-
-
 
 function App() {
   return (
     <div className="App">
-      {/* <AppForExercices /> */}
-      <AppForExerciceRecap />
+
     </div>
   );
 }
